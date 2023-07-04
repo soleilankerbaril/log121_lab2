5dfea723e4b3ef28442bae2b064844d203f95706
Peperooni <58051161+Peperooni@users.noreply.github.com>
2023-07-04 11:13:25 -0400

    Add Patron Observater Classes

---
+++ src/Patron/Observer.java
@@ -0,0 +1,5 @@
+package Patron;
+
+public interface Observer {
+    public void update();
+}
---
+++ src/Patron/Subject.java
@@ -0,0 +1,23 @@
+package Patron;
+
+import java.util.ArrayList;
+
+public class Subject {
+    ArrayList<Observer> observers;
+
+    public Subject() {
+        observers = new ArrayList<Observer>();
+    }
+    public void attach(Observer o){
+        observers.add(o);
+    }
+    public  void dettach(Observer o){
+        observers.remove(o);
+    }
+
+    public void notifyObservers(){
+        for(Observer o: observers) {
+            o.update();
+        }
+    }
+}
